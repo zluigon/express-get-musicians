bb2b4d3280f75f144f7e7ebe850ae22f67288952
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-23 11:17:21 -0400

    Update: Part 1 - GET w/ tests completed

--- index.test.js
+++ index.test.js
@@ -1,22 +1,33 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  // Write your tests here
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+  test("testing musicians endpoint statusCode", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
 
+  test("testing musicians endpoint response", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
 
-
-
-    
-})
+    expect(responseData[0]).toEqual(
+      expect.objectContaining({
+        id: 1,
+        name: "Mick Jagger",
+        instrument: "Voice",
+        bandId: null,
+      })
+    );
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
 
-
-
-
-
-
+app.get("/musicians", async (req, res) => {
+  const allMusicians = await Musician.findAll();
+  res.json(allMusicians);
+});
 
 module.exports = app;
